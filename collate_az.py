13a2c7ecfdd1af3eeea23923089d67d26ed5b87b
Jeremy Muhlich <jmuhlich@bitflood.org>
2016-09-13 10:31:25 -0400

    Refined az script - better sorting and logging output

--- collate_az.py
+++ collate_az.py
@@ -4,7 +4,9 @@ import re
 import itertools
 import collections
 import zipfile
+import datetime
 import pandas as pd
+import dateutil
 from unipath import Path
 
 
@@ -13,26 +15,21 @@ def finddupe(a):
 
 
 def fixplateintegrity(df):
-    # cycle through the rows
+    # FIXME this should merge with a full list of wellnames rather than
+    # iterate, as the current code will mishandle the case where both replicate
+    # wells are missing.
     letters = 'CDEFGHIJKLMN'
-    numbers = range(3,22+1)
-    print 'DF Shape checking', df.shape
+    numbers = [str(i) for i in range(3, 22+1)]
     for indx, wellname in enumerate(itertools.product(letters,numbers)):
-        wellname = wellname[0] + str(wellname[1])
+        wellname = wellname[0] + wellname[1]
         if wellname != df['WellName'][indx]:
-            print 'Indx %d InternalWN %s ReadWN %s' % (indx, wellname, df['WellName'][indx])
-            print 'Problem found: fixing plate'
-
+            print ('        !! Missing data for well %s; copying from'
+                   ' replicate' % wellname)
             if indx % 2 == 0:
-	        # even
-	        #newrow = df.iloc[indx].values
 	        df = pd.concat([df.iloc[0:indx+1], df.iloc[indx:]], axis=0)
             else:
-	        #newrow = df.iloc[indx-1]
 	        df = pd.concat([df.iloc[0:indx], df.iloc[indx-1:]], axis=0)
             df = df.reset_index(drop=True)
-
-            print 'Fixed missing value ->', df.shape
     return df
 
 def subdirs(path):
@@ -59,17 +56,25 @@ df = {}
 plate_df_r1 = []
 plate_df_r234 = []
 
+print "Reading experimental design\n==========\n"
+
 platefilelist = ['Plate layout_AZcpds&liver-kidney_Rep1.xlsx',
                  'Plate layout_AZcpds&liver-kidney_Rep2-4.xlsx']
 for pl_df, pl_name in zip((plate_df_r1, plate_df_r234), platefilelist):
     pl_path = input_path.child(pl_name)
+    print "File:", pl_path
     for sheet in (0, 1):
+        print "  sheet", sheet
         pl_sheet = pd.read_excel(pl_path, sheet)
         pl_sheet.Row = pl_sheet.Row.astype(unicode)
         pl_sheet.Column = pl_sheet.Column.astype(unicode)
         pl_df.append(pl_sheet)
 
+
+print "\n\nReading data files\n==========\n"
+
 for zpath in zippaths:
+    print "Scanning", zpath
     zfile = zipfile.ZipFile(zpath)
     replicate = re.findall('_Rep(\d)', zpath)[0]
 
@@ -83,15 +88,12 @@ for zpath in zippaths:
     for plate_path in plate_paths:
         sim = re.findall('(Sim_00000\d)', plate_path)[0]
 
-        print 'Found', zpath, 'over', plate_path
-        print 'R', replicate, 'Sim', sim,
+        print "Searching for plate data in", plate_path
+        print "(Rep: %s  Plate: %s)" % (replicate, sim)
 
         df[replicate].setdefault(sim, [])
 
-        # Num is used to find the right plate layout file
-
-        # Go through files
-
+        # Find csv files under plate_path, but skip the ones listed in badpaths.
         timepoint_paths = [p for p in zfile.namelist()
                            if p.startswith(plate_path) and p.endswith('.csv')
                            and p not in badpaths]
@@ -100,38 +102,51 @@ for zpath in zippaths:
         assert num_timepoints == 8, ("Expected 8 timepoint .csv files, found %d"
                                      % num_timepoints)
 
+        # Here we rely on having an ISO8601 timestamp in the paths so that
+        # lexically sorting them puts them in time-course order. We'll still
+        # verify our assumption later by inspecting the timestamps inside the
+        # files, but this is a nice shortcut.
+        timepoint_paths = sorted(timepoint_paths)
+        timestamp0 = timepoint_paths[0].split('/')[2][:22]
+        t0 = dateutil.parser.parse(timestamp0)
+
         for csvpath in timepoint_paths:
 
-            print 'csvfile', csvpath
+            timestamp = csvpath.split('/')[2][:22]
+            t = dateutil.parser.parse(timestamp)
+            delta_t = t - t0
+
+            delta_t_minutes_s = delta_t.total_seconds() // 60 * 60
+            delta_t_minutes = datetime.timedelta(seconds=delta_t_minutes_s)
+            print '   %s @ %s' % (delta_t_minutes, csvpath.split('/', 2)[-1])
             # Specify 'str' as dtype to prevent any parsing of floats etc. to
             # preserve original values exactly.
             tempdf = pd.read_csv(zfile.open(csvpath), encoding='utf-8', dtype='str')
             tempdf = fixplateintegrity(tempdf)
-
-            # check file integrity
+            assert len(tempdf) == 240, ('Expected 240 rows, found %d'
+                                        % len(tempdf))
 
             #For sim1 or sim2
             #Add a time column
             #Add a replicate column, but ignore the sim, we will join sim1 and 3 and sim will not be needed
-            print tempdf.shape
 
+            # Prepend experimental design and replicate number to Structural
+            # Panel plates; Functional Panel plates will be merged later.
             if sim == 'Sim_000001' or sim == 'Sim_000002':
 
                 if sim == 'Sim_000001':
                     pl = plate_df[0]
                 if sim == 'Sim_000002':
                     pl = plate_df[1]
-
-                tempdf['ReplicateNumber'] = replicate
+                pl = pl.copy()
+                pl['ReplicateNumber'] = replicate
 
                 assert len(tempdf) == len(pl), "design-experiment mismatch"
                 tempdf = pl.merge(tempdf, on=['WellName', 'Row', 'Column'])
                 assert len(tempdf) == len(pl), "design merge failure"
 
-            print 'Adding to df with %s and %s' % (replicate, sim), 'Shape', tempdf.shape
             df[replicate][sim].append(tempdf)
 
-import pdb; pdb.set_trace()
 #Ok all read in, now go through each replicate (1 to 4)
 #For each sim1 item, join it to corresponding sim3 item, this is replicate X, compounds Y-Z, 7 time points, full set of struct and func features
 
@@ -153,41 +168,37 @@ def drop_duplicate_columns(df):
     return df.iloc[:, keep]
 
 
+print "\n\nMerging plates\n==========\n"
+
 for replicate, rdf in df.items():
 
-    print 'Checking replicate %s' % replicate
-    print 'Length', len(rdf['Sim_000001'])
+    print "Replicate", replicate
 
-    final = pd.DataFrame()
+    final = None
 
     for panel_a, panel_b in (('Sim_000001', 'Sim_000003'),
                              ('Sim_000002', 'Sim_000004')):
-        for df1,df2 in zip(rdf[panel_a],rdf[panel_b]):
 
-            print 'Adding for set A with shapes', df1.shape, df2.shape
+        for tp, (df1, df2) in enumerate(zip(rdf[panel_a], rdf[panel_b])):
+
+            print "    Timepoint %d: %s / %s" % (tp, panel_a, panel_b)
             df2.columns = [x + '_2' for x in df2.columns.values]
             tempdf = pd.concat([df1, df2], axis=1)
             tempdf = drop_duplicate_columns(tempdf)
-            final.append(tempdf)
+            if final is None:
+                final = tempdf
+            else:
+                assert (tempdf.columns == final.columns).all(), "column mismatch"
+                final = final.append(tempdf)
 
-    for df1,df2 in zip(rdf['Sim_000002'],rdf['Sim_000004']):
-        print 'Adding for set B'
-        df2.columns = [x + '_2' for x in df2.columns.values]
-        tempdf = pd.concat([df1, df2], axis=1)
-        tempdf = drop_duplicate_columns(tempdf)
-        if 'set 39-73' not in rdf:
-            rdf['set 39-73'] = tempdf
-        else:
-            print 'Pre drop shape', tempdf.shape
-            print 'Joining shapes', rdf['set 39-73'].shape, tempdf.shape
-            rdf['set 39-73'] = rdf['set 39-73'].append(tempdf)
+    final = final.sort_values(['MeasurementDate', 'PlateName'])
+    final = final.reset_index(drop=True)
+    assert final.shape == (3840, 691), "final merged table has wrong size"
 
-    finaldf = pd.concat([rdf['set 1-38'], rdf['set 39-73']], axis=0)
-    finaldf.index = range(0, len(finalrdf))
-
-    with open(output_path.child('set_'+replicate+'.csv'), 'w') as fp:
-        cols = finaldf.columns
+    final_path = output_path.child('Replicate_'+replicate+'.csv')
+    print "    Writing output to", final_path
+    with open(final_path, 'w') as fp:
+        cols = final.columns
         cols = [non_ascii(col) for col in cols]
-        finaldf.columns = cols
-        finaldf.to_csv(fp)
-        fp.write('# Raw data with all replicates, light processing replace blank wells with their technical replicate\n')
+        final.columns = cols
+        final.to_csv(fp, index=False)
